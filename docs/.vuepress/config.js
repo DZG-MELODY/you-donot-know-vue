cab104c111a670a19c881e1f712a63656e5f130e
董志刚 <dongzhigang_bit@126.com>
2019-06-17 23:59:31 +0800

    feat: finish preface feat: update vuepress config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,34 @@
+const path = require("path");
+
 module.exports = {
   base: "/you-donot-know-vue/",
   title: "你不知道的Vue",
   description: "换一个角度介绍你所不知道的Vue",
+  dest: path.resolve(__dirname, "../../dist/"),
   markdown: {
     lineNumbers: true
+  },
+  themeConfig: {
+    nav: [
+      { text: "序言", link: "/preface/" },
+      {
+        text: "正文",
+        items: [
+          { text: "MVVM", link: "/mvvm/mvvm-1.md" },
+          { text: "介绍", link: "/introduction/" }
+        ]
+      },
+      { text: "附录", link: "/appendix/" },
+      {
+        text: "Github",
+        link: "https://github.com/DZG-MELODY/you-donot-know-vue.git"
+      }
+    ],
+    sidebar: {
+      "/preface/": [""],
+      "/mvvm/": ["mvvm-1", "mvvm-2"],
+      "/introduction/": ["intro-general-1", "intro-vnode-1"],
+      "/": [""]
+    }
   }
 };
--- docs/README.md
+++ docs/README.md
@@ -1,12 +1,14 @@
 ---
 home: true
 heroImage: /hero.png
-actionText: 开始学习 →
-actionLink: /zh/guide/
+actionText: 开始阅读 →
+actionLink: /preface/
 features:
 - title: 全新角度
-  details: 以实际应用为出发点，从需求点开始逐本溯源，探究深层次的设计理念
+  details: 以实际应用为出发点，从一个个小的功能点开始逐本溯源，探究更广义的设计理念
 - title: 深入浅出
-  details: 不过分关注源代码，从简单功能点逐步深入探索vue的设计思想和实现原理
+  details: 不过分依赖源码结构，从简单功能点逐步深入探索vue的设计思想和实现原理
+- title: 更高视角
+  details: 源于Vue，但不局限于Vue，充满了对前端未来发展的畅想
 footer: MIT Licensed | Copyright © 2019-present DZG-MELODY
 ---
